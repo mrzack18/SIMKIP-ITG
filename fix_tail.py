ca7a8048122dd1d4bb22130951f999d3cc69160b
mrzack18 <2306094@itg.ac.id>
2026-09-07 18:05:49 +0700

    bersih bersih

--- fix_tail.py
+++
@@ -1,5 +0,0 @@
-with open('resources/js/components/modules/admin/mahasiswa/TabInfoPribadi.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-with open('resources/js/components/modules/admin/mahasiswa/TabInfoPribadi.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines[:-6])
